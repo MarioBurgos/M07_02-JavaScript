95feb7bc6efe4aebce2324dab9798fb502a170eb
Mario Burgos <mario.burpi@gmail.com>
2021-10-14 10:31:07 +0200

    Exercici 3 amb metode map()

--- Nivel 1/Ex3/lletres.js
+++ Nivel 1/Ex3/lletres.js
@@ -1,8 +1,19 @@
-var myName = ['M', 'a', 'r', 'i', 'o', ' ', 'e', 's', ' ', 'e', 'l', ' ', 'm', 'e', 'u', ' ', 'n', 'o', 'm'];
-var results = [];
+var myName = ['m', 'a', 'r', 'i', 'o', ' ', 'e', 's', ' ', 'e', 'l', ' ', 'm', 'e', 'u', ' ', 'n', 'o', 'm'];
+var results = {};
 
-myName.forEach(function(char) {
+/** Solucion 2 */
+
+results = myName.map(char => {
     results[char] = (results[char] || 0) + 1;
-
+    return results;
 });
-console.log(results);
+console.log(results[0]);
+
+
+
+/** Solucion 1 */
+// myName.forEach(char => {
+//     results[char] = (results[char] || 0) + 1;
+
+// });
+// console.log(results);
